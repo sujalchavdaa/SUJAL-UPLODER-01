ba59f4e8785bf4f01099589b265787036d91b069
sujalchavdaa <sujalchavda500@gmail.com>
2025-07-03 18:31:01 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,7 +7,7 @@ API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", "8118667253"))
-CREDIT = "✿𝐒𝐔𝐉𝐀𝐋✿"
+CREDIT = "🌸ྀིSᑌᒍᗩՆ🌸"
 
 AUTH_USER = os.environ.get('AUTH_USERS', '8118667253').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
