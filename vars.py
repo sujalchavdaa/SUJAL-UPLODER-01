0b1f7123ed7a9fc14da1918c1607f2d6aebf51ce
sujalchavdaa <sujalchavda500@gmail.com>
2025-07-03 18:17:23 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -7,11 +7,17 @@ API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER = int(environ.get("OWNER", "8118667253"))
-CREDIT = "🌸ྀིSᑌᒍᗩՆ🌸"
+CREDIT = "✿𝐒𝐔𝐉𝐀𝐋✿"
+
 AUTH_USER = os.environ.get('AUTH_USERS', '8118667253').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
+
+# ➕ MongoDB config (Render env se aayega)
+MONGO_URI = environ.get("MONGO_URI", "")
+DB_NAME = environ.get("DB_NAME", "Telegrambot")
+COLLECTION_NAME = environ.get("COLLECTION_NAME", "Telegrambot")
   
 #WEBHOOK = True  # Don't change this
 #PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
